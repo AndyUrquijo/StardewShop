f8a96c6148b83794e566de6ea677cafd65f8562a
Andres Urquijo <andres.urquijo@endlessadventuresinc.com>
2023-10-07 18:56:39 -0500

    Realized that the spritesheets include left animations so the flipping logic was deleted and those animations where included instead.

--- Assets/Scripts/SpriteSynchronizer.cs
+++ Assets/Scripts/SpriteSynchronizer.cs
@@ -15,24 +15,11 @@ public class SpriteSynchronizer : MonoBehaviour
     [SerializeField] SpriteSheet clothesSheet;
     [SerializeField] SpriteSheet hatSheet;
 
-	Animator animator;
-
-	void Awake()
-	{
-		animator = GetComponent<Animator>();
-	}
-
 	void Update()
 	{
 		if(bodySheet) body.sprite = bodySheet.GetSprite(x, y);
 		if(clothesSheet) clothes.sprite = clothesSheet.GetSprite(x, y);
 		if(hatSheet) hat.sprite = hatSheet.GetSprite(x, y);
-
-		float dirX = animator.GetFloat(PlayerMovement.ID_DirX);
-		bool flip = dirX < 0;
-		body.flipX = flip;
-		clothes.flipX = flip;
-		hat.flipX = flip;
 	}
 
 }
